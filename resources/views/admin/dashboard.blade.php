d8fc2cf9754f4c13ce0e72764b59e178c81f3ce0
alexander_bromin7 <alexander@bromin7.com>
2019-09-05 13:40:45 +0300

    Creat admin template

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,53 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p class="alert alert-primary text-center">Категорий 0</p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p class="alert alert-primary text-center">Материалов 0</p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p class="alert alert-primary text-center">Посетителей 0</p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p class="alert alert-primary text-center">Сегодня 0</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                    <a href="#" class="btn btn-block btn-light bg-white card-header">Создать категорию</a>
+                    <a href="" class="card-body text-secondary">
+                        <h4 class="card-title">Категория первая</h4>
+                        <p class="card-text">
+                            Кол-во материалов
+                        </p>
+                    </a>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <a href="#" class="btn btn-block btn-light bg-white card-header">Создать материал</a>
+
+                    <a href="" class="card-body text-secondary">
+                        <h4 class="card-title">Материал первый</h4>
+                        <p class="card-text">
+                            Категория
+                        </p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
